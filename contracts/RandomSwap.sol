aeb6620fab1c5eee7f0154b2ce23893d25707ecc
Matthieu SCARSET <m@matthieuscarset.com>
2021-10-29 21:35:12 +0200

    Remove useless tokenizer

--- contracts/RandomSwap.sol
+++ contracts/RandomSwap.sol
@@ -2,90 +2,53 @@
 pragma solidity ^0.8.2;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @custom:security-contact m@matthieuscarset.com
-contract RandomSwap is ERC20, ERC20Burnable, Ownable {
-    // List of swappers (e.g. balances).
-    mapping(address => uint256) private swappers;
+contract RandomSwap is Ownable {
+    // Allow arbritrary closing of the app.
+    bool public _lock;
 
     // List of last swaps' timestamps.
     mapping(address => uint256) private swaps;
 
     // Events.
-    event Deposit(address indexed _from, bytes32 indexed _id, uint256 _value);
-    event Withdraw(address indexed _from, uint256 _value);
-    event Swap(address indexed _from, uint256 _token0, uint256 _token1);
+    event Swap(address indexed _from, string _token, uint256 _amount);
+
+    // Modifiers.
+    modifier hasNotSwapToday() {
+        require(
+            block.timestamp >= (swaps[msg.sender] + 1 days),
+            "No more swap for you today"
+        );
+        _;
+    }
 
     // Deploy contract.
-    constructor() ERC20("RandomSwap", "RSWAP") {
-        console.log("Deploying RSWAP tokens (1000)");
-        _mint(msg.sender, 1000 * 10**decimals());
-    }
-
-    // God user can create tokens.
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
-
-    // Hello money.
-    function deposit(bytes32 _id) public payable {
-        uint256 _amount = msg.value;
-        uint256 _balance = swappers[msg.sender];
-        swappers[msg.sender] = _balance + _amount;
-
-        emit Deposit(msg.sender, _id, msg.value);
-    }
-
-    // Bye bye money.
-    function withdraw(uint256 _amount) public {
-        uint256 _balance = swappers[msg.sender];
-
-        // Check if enought cash, send it and update balance.
-        require(_balance >= _amount, "Not enough money");
-        this.transfer(msg.sender, _amount);
-        swappers[msg.sender] = _balance - _amount;
-
-        emit Withdraw(msg.sender, _amount);
-    }
+    constructor() payable {}
 
     // Make it swap!
-    function makeItSwap(uint256 _amount) public {
-        uint256 _now = block.timestamp;
-        uint256 _lastSwap = swaps[msg.sender];
-        uint256 _balance = swappers[msg.sender];
+    function swap(string memory _token, uint256 _amount)
+        public
+        payable
+        hasNotSwapToday
+    {
+        // Security check.
+        require(!_lock, "RandomSwap is closed at the moment");
 
-        // No money, no honey.
-        require(_balance > 0, "Empty balance");
-
-        // Have you already swapped today?
-        if (_lastSwap > 0) {
-            require(
-                _now >= (_lastSwap + 1 days),
-                "No more swap for you today."
-            );
+        // Check solvability.
+        if (msg.sender.balance < _amount) {
+            revert("Sorry not enought money, honey");
         }
 
-        // Enought cash?
-        require(_balance >= _amount, "Not enough money");
-
-        // @todo Prepare source and target token.
-        uint256 _token0;
-        uint256 _token1;
+        // @todo Randomly select a token.
 
         // @todo Make it swap now.
 
         // Lock swapper for today.
-        swaps[msg.sender] = _now;
+        swaps[msg.sender] = block.timestamp;
 
-        // @todo trigger event.
-        emit Swap(msg.sender, _token0, _token1);
-    }
-
-    // Check one's balance.
-    function getBalance(address _from) public view returns (uint256) {
-        return swappers[_from];
+        // Tell the world about this swap.
+        emit Swap(msg.sender, _token, _amount);
     }
 }
--- test/test.js
+++ test/test.js
@@ -1,19 +1,37 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("RandomSwap", function () {
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
+    const [owner] = await ethers.getSigners();
+    const RandomSwap = await ethers.getContractFactory("RandomSwap");
+    const rswap = await RandomSwap.deploy();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const ownerBalance = await rswap.balanceOf(owner.address);
+    expect(await rswap.totalSupply()).to.equal(ownerBalance);
+  });
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  it("Owner should be able to transfer RWSAP tokens", async function () {
+    const RandomSwap = await ethers.getContractFactory("RandomSwap");
+    const rswap = await RandomSwap.deploy();
+    await rswap.deployed();
+
+    const [owner, guy] = await ethers.getSigners();
+    await rswap.transfer(guy.address, 50);
+    const payeeCash = await rswap.balanceOf(owner.address);
+    expect(ethers.utils.formatEther(payeeCash)).to.equal('50.0');
+
+    /*
+
+    // Deposit test.
+    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
     // wait until the transaction is mined
-    await setGreetingTx.wait();
+    // await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // expect(await greeter.greet()).to.equal("Hola, mundo!");
+    */
   });
+
+
 });
